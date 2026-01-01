001aa7f414a64137acf7cd6f2f7f326880bb213f
flkrdze <tarasovruslan815@gmail.com>
2026-01-01 22:48:45 +0300

    add Transaction class

---
+++ ConsoleFinanceTracker/Transaction.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleFinanceTracker
+{
+    public class Transaction
+    {
+        public decimal Amount { get; set; }
+        public string Description { get; set; }
+        public DateTime Date {  get; set; }
+
+        public Transaction(decimal amount, string descrption)
+        {
+            Amount = amount;
+            Description = descrption;
+        }
+
+        public override string ToString()
+        {
+            return $"{Date} | {Amount} | {Description}";
+        }
+    }
+}
